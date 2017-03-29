2370790575a7987d9966ffc91082c5eddadf7c7c
Dale Harris <rodmur@gmail.com>
2017-03-29 19:05:25 -0400

    okay, finally got roads and libraries correct

--- roadslibraries.py
+++ roadslibraries.py
@@ -8,19 +8,18 @@ def DFS(G, v, visited):
     while S:
         v = S.pop()
         if not visited[v]:
+            edges += 1
             visited[v] = True
             for w in G[v]:
                 S.append(w)
-        else:
-            edges += 1
-    return edges 
+    return edges - 1
 
     
 q = int(input())
 for i in range(q):
-    n,m,x,y = map(int, input().split())
-    if x <= y:
-        print(n*x)
+    n,m,libCost,roadCost = map(int, input().split())
+    if libCost < roadCost:
+        print(n*libCost)
         for _ in range(m):
             _ = input()
         continue
@@ -29,17 +28,14 @@ for i in range(q):
     for _ in range(m):
         city_1,city_2 = map(int, input().split())
         graph[city_1] += [city_2]
+        graph[city_2] += [city_1]
     
     visited = { k: False for k in range(1,n+1) }
     roads = 0
     libraries = 0
-    vertex = 1
-    while sum(visited.values()) < n:
-        roads += DFS(graph, vertex, visited)
-        libraries += 1
-        for k,v in visited.items():
-            if v is False:
-                vertex = k
-                break
+    for vertex in range(1,n+1):
+        if not visited[vertex]:
+            libraries += 1
+            roads += DFS(graph, vertex, visited)
 
-    print(libraries * x + roads * y)
+    print(libraries * libCost + roads * roadCost)
